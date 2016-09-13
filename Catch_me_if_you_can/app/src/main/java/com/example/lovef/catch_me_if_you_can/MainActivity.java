de90da25a959a1283245d660ea7fe767c6dbbca6
이도겸 <lkc2252@gmail.com>
2016-09-13 16:22:26 +0900

    최초커밋

---
+++ Catch_me_if_you_can/app/src/main/java/com/example/lovef/catch_me_if_you_can/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lovef.catch_me_if_you_can;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//http://blog.naver.com/PostView.nhn?blogId=pahapck0&logNo=220586927852
+    //너무나 잘 설명되어있는 android 프로젝트 구조
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ Catch_me_if_you_can/app/src/test/java/com/example/lovef/catch_me_if_you_can/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.lovef.catch_me_if_you_can;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
